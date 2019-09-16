8a78417a7c4b76c8eebead7563f63dbe40fe914b
alatos2 <alabitosin60@gmail.com>
2019-09-16 19:07:48 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from earthquake alert system!</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
